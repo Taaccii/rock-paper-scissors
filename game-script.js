406fc66d7d292921a161f8e5e470a101092ae8e0
Taaccii <tom1991@hotmail.it>
2026-02-07 13:14:03 +0100

    feat: add humanchoice and playrounf logic

--- game-script.js
+++ game-script.js
@@ -1,4 +1,5 @@
 //Random CPU Choice for rock, paper and scissors 
+
 function getComputerChoice() {
 
   let randomChoice = Math.random();
@@ -15,4 +16,47 @@ function getComputerChoice() {
 }
 
 
-TODO //Player choice
+//Human choice
+
+function getHumanChoice() {
+
+  let choice = prompt("Rock, paper or scissors?", "Insert only rock paper or scissors")
+
+  if (choice.toLowerCase() === "rock" ) {
+    return "rock";
+  } else if (choice.toLocaleLowerCase() === "paper") {
+    return "paper";
+  } else if (choice.toLocaleLowerCase() === "scissors") {
+    return "scissors";
+  } else return getHumanChoice();
+  
+}
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+const umanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+
+// First round logic
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === "paper" && computerChoice === "rock") {
+    humanScore++;
+    console.log("You Win! Paper beats Rock")
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    humanScore++;
+    console.log("You Win! Rock beats Scissors")
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    humanScore++;
+    console.log("You Win! Scissors beats Paper")
+  } else {
+    computerScore++;
+    console.log("Youe Lose!")
+  }
+}
+
+playRound(umanSelection, computerSelection)
+
+console.log(playRound())
